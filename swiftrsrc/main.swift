6a85f6292817de469bc0e5b7ef0c04f20f565649
Indragie Karunaratne <i@indragie.com>
2015-01-25 22:34:52 -0700

    Initial Commit

---
+++ swiftrsrc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftrsrc
+//
+//  Created by Indragie on 1/25/15.
+//  Copyright (c) 2015 Indragie Karunaratne. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
