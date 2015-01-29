8bfd3138116eb8274c55f6af87b943e1c77ad970
Indragie Karunaratne <i@indragie.com>
2015-01-29 11:31:55 -0700

    Code generation for storyboards

---
+++ swiftrsrc/Storyboard.swift
@@ -0,0 +1,76 @@
+//
+//  Storyboard.swift
+//  swiftrsrc
+//
+//  Created by Indragie on 1/29/15.
+//  Copyright (c) 2015 Indragie Karunaratne. All rights reserved.
+//
+
+import Foundation
+
+/// Storyboard file (*.storyboard)
+struct Storyboard {
+    let URL: NSURL
+    let name: String
+    private let document: NSXMLDocument
+    
+    init?(URL: NSURL, error: NSErrorPointer) {
+        if let document = NSXMLDocument(contentsOfURL: URL, options: .allZeros, error: error) {
+            self.document = document
+            self.URL = URL
+            self.name = URL.fileName!
+        } else {
+            return nil
+        }
+    }
+}
+
+// MARK: Printable
+
+extension Storyboard: Printable {
+    var description: String {
+        return "Storyboard{name=\(name), URL=\(URL)}"
+    }
+}
+
+// MARK: CodeGeneratorType
+
+extension Storyboard: CodeGeneratorType {
+    func generateCode(#nested: Bool) -> String {
+        let stringsForXPath: String -> [String] = {
+            let nodes = self.document.nodesForXPath($0, error: nil) as [NSXMLNode]
+            return mapSome(nodes, { $0.stringValue })
+        }
+        
+        let constantDeclaration: String -> String = {
+            return "static let \($0.camelCaseString) = \"\($0)\"\n"
+        }
+        
+        let storyboardIdentifiers = stringsForXPath("//@storyboardIdentifier")
+        let reuseIdentifiers = stringsForXPath("//@reuseIdentifier")
+        let segueIdentifiers = stringsForXPath("//segue/@identifier")
+        
+        var code = "struct \(name.camelCaseString) {\n"
+        if nested {
+            let categories = [
+                ("StoryboardIdentifiers", storyboardIdentifiers),
+                ("ReuseIdentifiers", reuseIdentifiers),
+                ("SegueIdentifiers", segueIdentifiers)
+            ]
+            for (name, identifiers) in categories {
+                code += "\tstruct \(name)\n"
+                for id in identifiers {
+                    code += "\t\t" + constantDeclaration(id)
+                }
+                code += "\t}\n"
+            }
+        } else {
+            let allIdentifiers = storyboardIdentifiers + reuseIdentifiers + segueIdentifiers
+            for id in allIdentifiers {
+                code += "\t" + constantDeclaration(id)
+            }
+        }
+        code += "}"
+        return code
+    }
+}
